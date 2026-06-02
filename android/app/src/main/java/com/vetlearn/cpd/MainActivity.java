a4cb2745b274620a426a8bc82ca61600bc05435f
drcraigbailey <drcraig.bailey@gmail.com>
2026-06-02 20:22:31 +0100

    Create Android notification channel on startup

--- android/app/src/main/java/com/vetlearn/cpd/MainActivity.java
+++ android/app/src/main/java/com/vetlearn/cpd/MainActivity.java
@@ -1,5 +1,32 @@
 package com.vetlearn.cpd;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.os.Build;
+import android.os.Bundle;
+
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        createNotificationChannel();
+    }
+
+    private void createNotificationChannel() {
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.O) return;
+
+        NotificationChannel channel = new NotificationChannel(
+            "vetlearn_messages",
+            "VetLearn messages",
+            NotificationManager.IMPORTANCE_DEFAULT
+        );
+        channel.setDescription("VetLearn message and activity notifications");
+
+        NotificationManager manager = getSystemService(NotificationManager.class);
+        if (manager != null) {
+            manager.createNotificationChannel(channel);
+        }
+    }
+}
