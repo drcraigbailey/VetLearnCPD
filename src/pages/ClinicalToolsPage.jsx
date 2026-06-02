635ef28621e0c4e7fa1c8a499693bdc102553ad6
drcraigbailey <drcraig.bailey@gmail.com>
2026-06-02 16:31:34 +0100

    Pass protocol context into clinical tools

--- src/pages/ClinicalToolsPage.jsx
+++ src/pages/ClinicalToolsPage.jsx
@@ -1,9 +1,13 @@
+import { useCallback, useState } from "react";
 import { Calculator } from "lucide-react";
 import PageBanner from "../components/PageBanner";
 import ProtocolContextSelector from "../components/ProtocolContextSelector";
 import ClinicalTools from "./ClinicalTools";
 
 export default function ClinicalToolsPage({ user, darkMode = false }) {
+  const [protocolContext, setProtocolContext] = useState(null);
+  const handleProtocolChange = useCallback((nextProtocol) => setProtocolContext(nextProtocol), []);
+
   return (
     <div className="space-y-6">
       <PageBanner
@@ -12,8 +16,8 @@ export default function ClinicalToolsPage({ user, darkMode = false }) {
         darkMode={darkMode}
         badges={[{ label: "Clinical calculators", icon: <Calculator size={14} />, accent: true }]}
       />
-      <ProtocolContextSelector user={user} darkMode={darkMode} />
-      <ClinicalTools user={user} darkMode={darkMode} showBanner={false} />
+      <ProtocolContextSelector user={user} darkMode={darkMode} onProtocolChange={handleProtocolChange} />
+      <ClinicalTools user={user} darkMode={darkMode} showBanner={false} protocolContext={protocolContext} />
     </div>
   );
 }
