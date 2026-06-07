62d48a8f9bf09e7ce3c9f05ebb07bd125c3e65b1
drcraigbailey <drcraig.bailey@gmail.com>
2026-06-07 12:16:37 +0100

    Initialize privacy mode runtime

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,10 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { registerPwaUpdates } from './pwaUpdate.js'
+import { initPrivacyModeRuntime } from './utils/privacyMode.js'
 
 registerPwaUpdates()
+initPrivacyModeRuntime()
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
