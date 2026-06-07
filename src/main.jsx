70c5791c6044bb3042909a5dc0afbcd4e4b1cf32
drcraigbailey <drcraig.bailey@gmail.com>
2026-06-07 15:25:01 +0100

    Start session inactivity security monitor

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,10 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { registerPwaUpdates } from './pwaUpdate.js'
+import { startSessionSecurity } from './utils/sessionSecurity.js'
 
 registerPwaUpdates()
+startSessionSecurity()
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
